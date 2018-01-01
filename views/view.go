5873a3b80b71ca3af66b4e4971a5da82b0705a46
Razwan Rashidi <razwan.izmi@gmail.com>
2018-01-01 20:02:33 +0800

    Create a controller for static pages, and updated the view type to implement the http handler interface

--- views/view.go
+++ views/view.go
@@ -30,6 +30,13 @@ type View struct {
 	Layout   string
 }
 
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := v.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
 // Render is used to render the view with the predefined layout.
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
