ce6059cb19b9daba09842a6e38f8548091655139
Razwan Rashidi <razwan.izmi@gmail.com>
2018-01-01 13:26:31 +0800

    Create a users controller and update main.go to use it instead of the signup function

---
+++ controllers/users.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+
+	"lenslocked.com/views"
+)
+
+// NewUsers is used to create a new Users controller
+// This function will panic if the templates are not
+// parsed correctly, and should only be used during
+// initial setup.
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+// New is used to render the form where a user can create
+// a new user account
+//
+// GET /signup
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// Create is used to process the signup form when a user tries to
+// create a new user account
+//
+// POST /signup
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a fake message. Pretend that we created a user account")
+}
