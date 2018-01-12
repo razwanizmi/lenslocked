54445d5452638f90f4564045fc06e97f19b9c0a6
Razwan Rashidi <razwan.izmi@gmail.com>
2018-01-12 08:18:39 +0800

    Change user create to hash password

--- models/users.go
+++ models/users.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -84,6 +86,12 @@ func (us *UserService) Close() error {
 // Create will create the provided user and backfill data
 // like the ID, CreatedAt, UpdatedAt fields.
 func (us *UserService) Create(user *User) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.PasswordHash = string(hashedBytes)
+	user.Password = ""
 	return us.db.Create(user).Error
 }
 
@@ -121,6 +129,8 @@ func (us *UserService) AutoMigrate() error {
 
 type User struct {
 	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
